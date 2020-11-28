ce58512d5fc5086ea159f0b404e7e57543a57f62
CodingGiraffe <adamthomas03@gmail.com>
2020-11-28 14:34:42 -0600

    first coding challange

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+console.log(calcAverage(3, 4, 5));
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log('Dolphins win');
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log('Koalas win');
+  } else {
+    console.log('Nobody wins');
+  }
+};
+
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(100, 5);
